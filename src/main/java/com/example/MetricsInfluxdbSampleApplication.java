44edca454a9c8e4adfcfc0f279ce70bce938750c
Sebastien Bortolussi <sebastien.bortolussi@orange.com>
2017-11-13 10:43:38 +0100

    Move to Micrometer
    These changes move metrics gathering and export to Micrometer, an application metrics facade that supports numerous monitoring systems, such as influxdb.
    
    [#3]

--- src/main/java/com/example/MetricsInfluxdbSampleApplication.java
+++ src/main/java/com/example/MetricsInfluxdbSampleApplication.java
@@ -1,21 +1,16 @@
 package com.example;
 
-import org.influxdb.InfluxDB;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
-import org.springframework.boot.actuate.endpoint.MetricsEndpoint;
-import org.springframework.boot.actuate.endpoint.MetricsEndpointMetricReader;
-import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
-import org.springframework.boot.actuate.metrics.writer.GaugeWriter;
-import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jmx.export.MBeanExporter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class MetricsInfluxdbSampleApplication {
@@ -23,40 +18,23 @@ public class MetricsInfluxdbSampleApplication {
     @Value("${cloud.application.instance_id:app_01}")
     private String appInstanceId;
 
-    @Autowired
-    private InfluxDB influxDB;
-
-
     public static void main(String[] args) {
         SpringApplication.run(MetricsInfluxdbSampleApplication.class, args);
     }
 
-    @Bean
-    @ExportMetricWriter
-    MetricWriter metricWriter(MBeanExporter exporter) {
-        return new JmxMetricWriter(exporter);
-    }
-
-    @Bean
-    @ExportMetricWriter
-    GaugeWriter influxMetricsWriter() {
-        InfluxDBMetricWriter.Builder builder = new InfluxDBMetricWriter.Builder(influxDB);
-        builder.appInstanceId(appInstanceId);
-        builder.batchActions(500);    // number of points for batch before data is sent to Influx
-        return builder.build();
-    }
-
-    @Bean
-    public MetricsEndpointMetricReader metricsEndpointMetricReader(MetricsEndpoint metricsEndpoint) {
-        return new MetricsEndpointMetricReader(metricsEndpoint);
-    }
 }
 
 @RestController
 class GreetingController {
 
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", defaultValue = "world !") String name) {
+    @Timed(quantiles = {0.5, 0.85, 0.95, 0.99})
+    public String greeting(@RequestParam(value = "name", defaultValue = "world !") String name) throws Exception {
+        final CountDownLatch latch = new CountDownLatch(1);
+        latch.await(1, TimeUnit.SECONDS);
+        if (new Random().nextInt(100) > 90) {
+            throw new Exception("Error (random) while handling request.");
+        }
         return String.format("hello %s", name);
     }
 
